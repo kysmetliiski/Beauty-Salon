e2152b10355467fa027bf353ecbca3b227d41e9c
kysmetliiski <19507@uktc-bg.com>
2023-11-30 11:21:49 +0200

    mvc work

--- controller/schedule.php
+++ controller/schedule.php
@@ -1,65 +1,19 @@
-<?php
+<?php 
+
 class schedule extends Controller
 {
-	function __construct()
+    function __construct()
     {
         
         parent::__construct();
     }
     public function index()
     {
+ 
 
-      $user = $this->model->fetch("SELECT * FROM uktclog WHERE email = ? AND password = ?",[ $this->email, $this->password ]);
-
-
-$date = @$_GET['date'];
-
-if ( !$date )
-    $date = date("Y-m-d");
-
-
-$stmt = $connection->prepare("
-    SELECT vreservations.*, uktclog.name, uktclog.lname
-    FROM vreservations 
-    LEFT JOIN uktclog ON uktclog.id = vreservations.user_id
-    WHERE datachas=?
-");
-$stmt->execute([$date]); 
-$data = $stmt->fetchAll();
-
-$nextdate = date('Y-m-d', strtotime('+1 day', strtotime($date)));
-$prevdate = date('Y-m-d', strtotime('-1 day', strtotime($date)));
-
-
-$data = $this->model->fetchAll("
-SELECT vreservations.*, uktclog.name, uktclog.lname
-FROM vreservations 
-LEFT JOIN uktclog ON uktclog.id = vreservations.user_id
-WHERE datachas=?
-",[$date]);
-
-
-
-
-
-
-
-
-
-   
-
-$i = 1;
-foreach( $data as $item ) {
-
-
-   
-    $i++;
+        $this->view->render("schedule.html");
+    }
 }
 
-   
-  
 
-$this->view->render("schedule.html");
-	}
-}
-?>
+?> 
